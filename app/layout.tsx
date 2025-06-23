7ae8ab83a58846b2a8f75b9fc9a1a7e27c4101d2
Hammad Majid <hammadmajid@proton.me>
2025-06-24 03:14:25 +0500

    feat: add CssBaseline to RootLayout for consistent styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/utils/theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const roboto = Roboto({
 	weight: ["300", "400", "500", "700"],
@@ -25,7 +26,12 @@ export default function RootLayout({
 		<html lang="en" className={roboto.variable}>
 			<body>
 				<AppRouterCacheProvider>
-					<ThemeProvider theme={theme}>{children}</ThemeProvider>
+					<ThemeProvider theme={theme}>
+						<>
+							<CssBaseline />
+							{children}
+						</>
+					</ThemeProvider>
 				</AppRouterCacheProvider>
 			</body>
 		</html>
