9034462d9eb77991d0380f256dc849d0606349d1
Hammad Majid <hammadmajid@proton.me>
2025-06-29 23:48:17 +0000

    feat: enhance inventory router with type annotations

--- server/routers/inventory.ts
+++ server/routers/inventory.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/trpc";
 import { PRODUCT_BY_SLUG_QUERY, PRODUCTS_QUERY } from "@/lib/queries";
+import { Product } from "@/types/product";
 
 export const inventoryRouter = createTRPCRouter({
     hello: publicProcedure
@@ -13,14 +14,12 @@ export const inventoryRouter = createTRPCRouter({
         }),
 
     getBySlug: publicProcedure.input(z.string()).query(
-        async ({ ctx, input }) => {
-            const item = await ctx.client.fetch(PRODUCT_BY_SLUG_QUERY, { input })
-
-            return item;
+        async ({ ctx, input }): Promise<Product> => {
+            return await ctx.client.fetch(PRODUCT_BY_SLUG_QUERY, { input })
         }
     ),
 
-    getAll: publicProcedure.query(async ({ ctx }) => {
+    getAll: publicProcedure.query(async ({ ctx }): Promise<Product[]> => {
         return await ctx.client.fetch(PRODUCTS_QUERY);
     })
 })
