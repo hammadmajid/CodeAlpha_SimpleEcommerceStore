f9823c79394eab03f53572b665d9aca95574e539
Hammad Majid <hammadmajid@proton.me>
2025-06-30 20:13:21 +0500

    fix: type cast the sanity fetch result to generated type and use new renamed context

--- server/routers/inventory.ts
+++ server/routers/inventory.ts
@@ -2,16 +2,14 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/trpc";
 import { PRODUCT_BY_SLUG_QUERY, PRODUCTS_QUERY } from "@/lib/queries";
-import { Product } from "@/types/product";
+import type { PRODUCTS_QUERYResult, PRODUCT_BY_SLUG_QUERYResult } from "@/sanity/types";
 
 export const inventoryRouter = createTRPCRouter({
-    getBySlug: publicProcedure.input(z.string()).query(
-        async ({ ctx, input }): Promise<Product> => {
-            return await ctx.client.fetch(PRODUCT_BY_SLUG_QUERY, { input })
-        }
-    ),
+    getBySlug: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
+        return await ctx.sanity.fetch(PRODUCT_BY_SLUG_QUERY, { input }) as PRODUCT_BY_SLUG_QUERYResult;
+    }),
 
-    getAll: publicProcedure.query(async ({ ctx }): Promise<Product[]> => {
-        return await ctx.client.fetch(PRODUCTS_QUERY);
-    })
-})
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        return await ctx.sanity.fetch(PRODUCTS_QUERY) as PRODUCTS_QUERYResult;
+    }),
+});
