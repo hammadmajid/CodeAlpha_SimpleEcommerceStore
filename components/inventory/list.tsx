14d11c5fa53a0e3e8f5027bd259ceddf1b436712
Hammad Majid <hammadmajid@proton.me>
2025-07-01 00:47:49 +0500

    style: improve product card layout and truncate long descriptions for fixed width

--- components/inventory/list.tsx
+++ components/inventory/list.tsx
@@ -21,9 +21,21 @@ export default function ProdutctList({ products }: ProdutctListProps) {
 	return (
 		<Grid container spacing={3}>
 			{products.map((product) => (
-				<Grid key={product._id}>
+				<Grid
+					key={product._id}
+					size={{
+						xs: 12,
+						sm: 6,
+						md: 4,
+						lg: 3
+					}}
+					display="flex"
+					justifyContent="center"
+				>
 					<Card
 						sx={{
+							width: "100%",
+							maxWidth: 420,
 							height: "100%",
 							display: "flex",
 							flexDirection: "column",
@@ -48,7 +60,10 @@ export default function ProdutctList({ products }: ProdutctListProps) {
 								<CardMedia
 									component="img"
 									height="250"
-									image={urlFor(product.images[0]).width(400).height(250).url()}
+									image={urlFor(product.images[0])
+										.width(400)
+										.height(250)
+										.url()}
 									alt={product.images[0].alt || product.name}
 									sx={{ objectFit: "cover" }}
 								/>
@@ -77,11 +92,16 @@ export default function ProdutctList({ products }: ProdutctListProps) {
 									<Typography
 										variant="body2"
 										color="text.secondary"
-										sx={{ mb: 2 }}
+										sx={{
+											mb: 2,
+											overflow: "hidden",
+											textOverflow: "ellipsis",
+											display: "-webkit-box",
+											WebkitLineClamp: 3,
+											WebkitBoxOrient: "vertical",
+										}}
 									>
-										{product.description.length > 100
-											? `${product.description.substring(0, 100)}...`
-											: product.description}
+										{product.description}
 									</Typography>
 								)}
 
